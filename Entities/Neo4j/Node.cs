b8c0e22c85fcd7aec7e8b65fe3301ef9c18c8738
dogabaris <dogabarisozdemir@gmail.com>
2020-07-13 01:26:24 +0300

    fix

--- Entities/Neo4j/Node.cs
+++ Entities/Neo4j/Node.cs
@@ -9,7 +9,7 @@ namespace WebApi.Entities.Neo4j
         [IgnoreDataMember]
         public long id { get; set; }
         public string workspace { get; set; }
-        public double data { get; set; }
+        public dynamic data { get; set; }
         public int x { get; set; }
         public int y { get; set; }
         public bool? expectedoutput { get; set; }
